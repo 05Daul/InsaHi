8d57cc386982a32936974a7abd7f8d84263baf3b
netioz <aij0513@naver.com>
2025-02-26 14:48:29 +0900

    test

---
+++ User/src/main/java/com/playdata/User/entity/Company.java
@@ -0,0 +1,19 @@
+package com.playdata.User.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Company") 
+@Getter
+@Setter
+@Builder
+public class Company {
+    
+}
+
