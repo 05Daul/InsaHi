606ccf6d18a2159ee247344ce82a3563d4e9cb77
Daul <167619579+05Daul@users.noreply.github.com>
2025-03-18 18:02:30 +0900

    Delete Meeting directory

--- Meeting/src/main/java/meeting/enums/ReservationStatus.java
+++
@@ -1,8 +0,0 @@
-package meeting.enums;
-
-import lombok.Getter;
-
-@Getter
-public enum ReservationStatus {
-    APPROVED, PENDING, COMPLETED
-}
--- Meeting/src/main/java/meeting/repository/MeetingRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package meeting.repository;
-
-import meeting.entity.MeetingRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, String> {
-}
