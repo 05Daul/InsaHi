2e862a2810c44be723c1f15cb824b4e8cea6d362
Kim Daul <elre519@naver.com>
2025-04-05 19:13:32 +0900

    Test2

--- ElectronicApproval/src/main/java/com/playdata/ElectronicApproval/common/publicEntity/DateEntity.java
+++ ElectronicApproval/src/main/java/com/playdata/ElectronicApproval/common/publicEntity/DateEntity.java
@@ -2,25 +2,25 @@ package com.playdata.ElectronicApproval.common.publicEntity;
 
 
 import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
 
 @Data
 @MappedSuperclass
 public class DateEntity {
 
-    @CreationTimestamp
-    private LocalDateTime createDate; // insert 날짜
-    @UpdateTimestamp
-    private LocalDateTime updatedDate; //업데이트 날짜
+  //1
+  @CreationTimestamp
+  private LocalDateTime createDate; // insert 날짜
+  @UpdateTimestamp
+  private LocalDateTime updatedDate; //업데이트 날짜
 
 
-    @CreationTimestamp
-    private LocalDateTime createTime; // insert 시간
-    @UpdateTimestamp
-    private LocalDateTime updateTime; // 업데이트 시간
+  @CreationTimestamp
+  private LocalDateTime createTime; // insert 시간
+  @UpdateTimestamp
+  private LocalDateTime updateTime; // 업데이트 시간
 
 }
