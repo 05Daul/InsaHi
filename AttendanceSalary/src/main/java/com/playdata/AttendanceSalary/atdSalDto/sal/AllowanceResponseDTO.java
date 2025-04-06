3f358e35bc1700c5ee1050348aa6ac30ecef96bd
Kim Daul <elre519@naver.com>
2025-04-06 23:54:12 +0900

    급여

--- AttendanceSalary/src/main/java/com/playdata/AttendanceSalary/atdSalDto/sal/AllowanceResponseDTO.java
+++ AttendanceSalary/src/main/java/com/playdata/AttendanceSalary/atdSalDto/sal/AllowanceResponseDTO.java
@@ -2,34 +2,34 @@ package com.playdata.AttendanceSalary.atdSalDto.sal;
 
 import com.playdata.AttendanceSalary.atdSalEntity.sal.AllowanceEntity;
 import com.playdata.AttendanceSalary.atdSalEntity.sal.AllowanceType;
-import com.playdata.AttendanceSalary.atdSalEntity.sal.PayStubEntity;
-//import com.playdata.attendanceSalary.atdSalEntity.sal.SalaryEntity;
+import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AllowanceResponseDTO {
-    private Long allowanceId;
-    private String companyCode;
-    private BigDecimal allowSalary;
-//    private PayStubEntity payStub;
-    private String allowName;
-    private String allowType;
 
-    public AllowanceEntity toEntity(){
-        AllowanceEntity allowanceEntity = new AllowanceEntity();
-        allowanceEntity.setAllowanceId(allowanceId);
-        allowanceEntity.setAllowanceId(this.allowanceId);
-        allowanceEntity.setCompanyCode(this.companyCode);
-        allowanceEntity.setAllowSalary(this.allowSalary);
-        allowanceEntity.setAllowType(AllowanceType.valueOf(allowType));
-        return allowanceEntity;
-    }
+  private Long allowanceId;
+  private String companyCode;
+  private BigDecimal allowSalary;
+  //    private PayStubEntity payStub;
+  private String allowName;
+  private String allowType;
+  private String employeeId;
+
+  public AllowanceEntity toEntity() {
+    AllowanceEntity allowanceEntity = new AllowanceEntity();
+    allowanceEntity.setAllowanceId(allowanceId);
+    allowanceEntity.setAllowanceId(this.allowanceId);
+    allowanceEntity.setCompanyCode(this.companyCode);
+    allowanceEntity.setAllowSalary(this.allowSalary);
+    allowanceEntity.setAllowType(AllowanceType.valueOf(allowType));
+    return allowanceEntity;
+  }
 
 }
